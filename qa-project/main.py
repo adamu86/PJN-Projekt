2584413e36465154050d053c82370618561d469a
TheLastPorg <34252031+TheLastPorg@users.noreply.github.com>
2026-01-13 21:18:51 +0100

    poprawne dzielenie na pasusy

--- qa-project/main.py
+++ qa-project/main.py
@@ -1,15 +1,28 @@
+import re
 from importlib.resources import path
 from bm25_engine import BM25Engine
 
 with open("data/regulamin.txt", encoding="utf-8") as f:
-    passages = [line.strip() for line in f if line.strip()]
+    text = f.read()
+
+paragraphs = re.split(r"\n(?=§\s*\d+)", text)
+passages = []
+
+for para in paragraphs:
+    parts = re.split(r"\n(?=\d+\.)", para)
+    if len(parts) == 1:
+        passages.append(" ".join(para.split()))
+    else:
+        for part in parts:
+            passages.append(" ".join(part.split()))
 
 engine = BM25Engine(passages)
 
-q = input("\nZadaj pytanie > ")
+while(1):
+    q = input("\nZadaj pytanie > ")
 
-results = engine.query(q)
-print("\nRanking:")
-for p, score in results:
-    print(f"[{score:.2f}] {p}")
-print("\n")
+    results = engine.query(q)
+    print("\nRanking:")
+    for p, score in results:
+        print(f"[{score:.2f}] {p}")
+    print("\n")
