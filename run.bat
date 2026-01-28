891be648b03f812dcb043afa242dd39d659723d2
Adam Grzeszczuk <103129308+adamu86@users.noreply.github.com>
2026-01-28 14:09:11 +0100

    cleanup

--- run.bat
+++ run.bat
@@ -1,6 +1,11 @@
 @echo off
 cd /d "%~dp0"
 
-start cmd /k "cd frontend && npm run dev"
+start cmd /k "cd frontend && npm install && npm run dev"
 
-start cmd /k "cd backend && uvicorn main:app --reload"
+python -m venv backend\venv
+
+call backend\venv\Scripts\activate.bat
+pip install -r backend\requirements.txt
+
+start cmd /k "cd backend && .\venv\Scripts\python.exe -m uvicorn main:app --reload"
